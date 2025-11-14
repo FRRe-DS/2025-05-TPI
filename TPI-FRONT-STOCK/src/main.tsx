5b23fb689e82ad19f241c01af74635b5399c3273
unknown <fariasvalentino25@gmail.com>
2025-11-14 19:06:25 -0300

    Initial home page

--- TPI-FRONT-STOCK/src/main.tsx
+++ TPI-FRONT-STOCK/src/main.tsx
@@ -2,20 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './client'
 import './styles/index.css'
 import App from './App.tsx'
-<<<<<<< HEAD
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './client'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-=======
-import { queryClient } from './client'
 import AppRouter from './routes/app.routes.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -26,6 +17,5 @@ createRoot(document.getElementById('root')!).render(
         </App>
       </QueryClientProvider>
     </BrowserRouter>
->>>>>>> 0da8b3b3a7cf9a0dded4b6ed1c55c8fbc75686e7
   </StrictMode>,
 )
