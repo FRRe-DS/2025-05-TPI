3492143c71a7a489b9d4504d443c26d07ffd38d5
unknown <fariasvalentino25@gmail.com>
2025-11-19 16:43:46 -0300

    merge fixed

--- TPI-FRONT-STOCK/src/main.tsx
+++ TPI-FRONT-STOCK/src/main.tsx
@@ -4,16 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import './styles/index.css'
 import App from './App.tsx'
-<<<<<<< HEAD
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './client'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-=======
 import { queryClient } from './client'
 import AppRouter from './routes/app.routes.tsx'
 
@@ -26,6 +16,5 @@ createRoot(document.getElementById('root')!).render(
         </App>
       </QueryClientProvider>
     </BrowserRouter>
->>>>>>> 0da8b3b3a7cf9a0dded4b6ed1c55c8fbc75686e7
   </StrictMode>,
 )
