9ca658c97fd3d2c5150600390d11d8e43424fe99
unknown <fariasvalentino25@gmail.com>
2025-11-20 15:35:37 -0300

    routes fixed

--- TPI-FRONT-STOCK/src/routes/app.routes.tsx
+++ TPI-FRONT-STOCK/src/routes/app.routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import ReservationsPage from "../pages/reservations";
 import Home from "../pages/home";
 import ProductList from "../pages/product";
@@ -9,6 +9,7 @@ export default function AppRouter(){
     <Routes>
       {/* Ruta principal - Muestra reservas directamente ya que no tenemos home */}
       <Route path="/" element={<Home/>}>
+        <Route index element={<Navigate to="/productos" replace />} />
         <Route path="/productos" element={<ProductList />}/>
         <Route path="/reservas" element={<ReservationsPage />}/>
         <Route path="/clientes" element={<ClientsPage />}/>
