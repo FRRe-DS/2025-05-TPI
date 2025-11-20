bb8801d30a586bea35f6aad7c12f6327a020315d
unknown <fariasvalentino25@gmail.com>
2025-11-20 00:55:54 -0300

    auth'nt

--- TPI-BACK-STOCK/src/routes/products.routes.ts
+++ TPI-BACK-STOCK/src/routes/products.routes.ts
@@ -10,30 +10,30 @@ const productController = container.productController;
 
 // GET /v1/productos - Listar todos los productos
 productRouter.get("/", 
-    requireAnyRole(["stock-be", "compras-be", "logistica-be"]), 
+    //requireAnyRole(["stock-be", "compras-be", "logistica-be"]), 
     productController.getAllProducts
 );
 
 // POST /v1/productos - Crear un nuevo producto
 productRouter.post("/", 
-    requireAnyRole(["stock-be"]), 
+   // requireAnyRole(["stock-be"]), 
     productController.createProduct
 );
 
 // GET /v1/productos/:productoId - Obtener un producto por ID
 productRouter.get("/:productoId", 
-    requireAnyRole(["stock-be", "compras-be", "logistica-be"]),
+    //requireAnyRole(["stock-be", "compras-be", "logistica-be"]),
     productController.getProductById
 );
 
 // PATCH /v1/productos/:productoId - Actualizar un producto
 productRouter.patch("/:productoId", 
-    requireAnyRole(["stock-be"]),
+    //requireAnyRole(["stock-be"]),
     productController.updateProduct
 );
 
 // DELETE /v1/productos/:productoId - Eliminar un producto
 productRouter.delete("/:productoId", 
-    requireAnyRole(["stock-be"]),
+    //requireAnyRole(["stock-be"]),
     productController.deleteProduct
 );
